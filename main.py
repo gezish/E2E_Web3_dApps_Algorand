25fb3d47debb43488382391e9ff1f598b1edea01
Gezish <gezish_lab@Gezahegn.localdomain>
2022-06-16 23:38:30 +0300

    main updated

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 @main.route('/profile')
 def profile():
-    return 'Profile'
+    return render_template('profile.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
